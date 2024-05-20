101865c0c0f285d7a5c735247d104ab02358dda1
Carlos Gerardo Martin <cm43833@gmail.com>
2024-05-20 14:49:10 +0100

    Working with audio play, make sure to pip install pydub.

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import numpy as np
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
 from sound.audio_handler import AudioHandler  # Import your AudioHandler class
+from pydub import AudioSegment
+from pydub.playback import play
 
 # Set the width and height of the radar grid
 width = 1335
@@ -17,6 +19,38 @@ point_coordinates = []
 drone_coordinates = []
 points_added = 0
 squares = []  # List to store coordinates of squares
+
+def play_music():
+    try:
+        # Load the audio file
+        sound = AudioSegment.from_file('./sound/US.wav')
+        
+        # Trim the audio to the first 2 seconds
+        sound = sound[:2000]  # 2000 ms = 2 seconds
+        
+        # Play the audio file
+        play(sound)
+    except Exception as e:
+        print(f"Error playing music: {e}")
+
+def droneWarning():
+    global frame
+    text = "Warning: Drone Detected!"
+
+    play_music()  # Play alert sound
+    
+    # Create a blank image
+    frame = np.zeros((1080, 1920, 3), dtype=np.uint8)
+    
+    # Add text to the image
+    cv2.putText(frame, text, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    
+    # Display the image in a window
+    cv2.imshow('Drone Detection Warning', frame)
+
+    # Wait for 5 seconds
+    cv2.waitKey(5000)  
+    cv2.destroyAllWindows()  # Close the window after waiting
 
 def pos_angle(base_angle, length):
     """
@@ -284,6 +318,8 @@ def run_radar_project(custom_model, radar_image, video, RESOLUTION_WIDTH, RESOLU
 
     audio.start()  # Open the stream
     audio.mainloop()
+
+    droneWarning()
 
     # Start the radar grid
     start_grid(radar_image)
